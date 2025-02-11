d0dc0bbd84e71681f24bb142605e93f1257d0e1d
dabdoubimeriem <dabdoubimeriem@gmail.com>
2025-02-11 17:33:20 +0100

    message

---
+++ Exo2/CompteBancaire.java
@@ -0,0 +1,24 @@
+package Exo2;
+
+public class CompteBancaire {
+    private String titulaire;
+    private int solde;
+    
+    
+    public void CompteBancaire (String titu, double sol){
+    this.titulaire = titu;
+    this.solde = sol;
+    }
+    
+    public void deposer(double montant){
+        if (montant > 0){
+           
+          
+            this.solde += montant;
+            System.out.println(this.titulaire + " a déposé" + montant);
+
+        }
+
+    }
+    
+}
