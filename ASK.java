1382b991884edf589eec550613c26ae3e65b9a8a
dabdoubimeriem <dabdoubimeriem@gmail.com>
2025-02-11 17:03:44 +0100

    message

--- ASK.java
+++ ASK.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 
-public class ASK {
-    public static void main(String[] args){
+public class Ask {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Entrez votre nom:");
+
+        System.out.print("Entrez votre nom : ");
         String nom = scanner.nextLine();
-        System.out.println("Bonjour," + nom + "!");
+
+        System.out.println("Bonjour "+nom+"!");
+
+        System.out.print("Entrez votre âge : ");
+        int age = scanner.nextInt();
+
+        System.out.println("Bonjour "+nom+" vous avez "+age+" ");
+
+
+
+
         scanner.close();
+
+
     }
 
-    
+   
 }
-
